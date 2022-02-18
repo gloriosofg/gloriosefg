373d0c369a097ec28bb143af0ebd64d2c998a555
gloriosofg <99756087+gloriosofg@users.noreply.github.com>
2022-02-18 17:20:32 -0300

    Add files via upload

---
+++ srln/Mayans M.C
@@ -0,0 +1,63 @@
+
+#EXTM3U
+
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S01 E01" tvg-logo="http://lxls.mx:8080/images/993e6e59ba2485331917ac78353e32f7.jpg" group-title="Séries | Star+",Mayans M.C. S01 E01
+http://lxls.mx:80/series/994153115/101217/212285.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S01 E02" tvg-logo="https://image.tmdb.org/t/p/w300/cWVWA1QffiHZRNkVeF46qygRbgJ.jpg" group-title="Séries | Star+",Mayans M.C. S01 E02
+http://lxls.mx:80/series/994153115/101217/212284.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S01 E03" tvg-logo="https://image.tmdb.org/t/p/w300/ywe1bpRtDbATR7AsXnOYTSuoXan.jpg" group-title="Séries | Star+",Mayans M.C. S01 E03
+http://lxls.mx:80/series/994153115/101217/212283.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S01 E04" tvg-logo="https://image.tmdb.org/t/p/w300/2ILjprkJr1hOK1ausXOz4bj7jac.jpg" group-title="Séries | Star+",Mayans M.C. S01 E04
+http://lxls.mx:80/series/994153115/101217/212282.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S01 E05" tvg-logo="https://image.tmdb.org/t/p/w300/oIdo5eChVIVcjOT94S0hNYvWDlS.jpg" group-title="Séries | Star+",Mayans M.C. S01 E05
+http://lxls.mx:80/series/994153115/101217/212281.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S01 E06" tvg-logo="https://image.tmdb.org/t/p/w300/onOmMgZaYxyc8ccWBf9jnkU2o0s.jpg" group-title="Séries | Star+",Mayans M.C. S01 E06
+http://lxls.mx:80/series/994153115/101217/212280.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S01 E07" tvg-logo="https://image.tmdb.org/t/p/w300/x9QCqfVtpEVTFp0m8G0Wj5P3Bai.jpg" group-title="Séries | Star+",Mayans M.C. S01 E07
+http://lxls.mx:80/series/994153115/101217/212279.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S01 E08" tvg-logo="http://lxls.mx:8080/images/993e6e59ba2485331917ac78353e32f7.jpg" group-title="Séries | Star+",Mayans M.C. S01 E08
+http://lxls.mx:80/series/994153115/101217/212278.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S01 E09" tvg-logo="https://image.tmdb.org/t/p/w300/rcfflNhtbo1IqGJtCZ8p6TF99CY.jpg" group-title="Séries | Star+",Mayans M.C. S01 E09
+http://lxls.mx:80/series/994153115/101217/212277.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S01 E10" tvg-logo="https://image.tmdb.org/t/p/w300/8xdl46gjjn2kGzQ4cpZ8vfwOEkT.jpg" group-title="Séries | Star+",Mayans M.C. S01 E10
+http://lxls.mx:80/series/994153115/101217/212276.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S02 E01" tvg-logo="https://image.tmdb.org/t/p/w300/pSayPm96PbnrFMA5ZuxN4ccL2J6.jpg" group-title="Séries | Star+",Mayans M.C. S02 E01
+http://lxls.mx:80/series/994153115/101217/207711.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S02 E02" tvg-logo="https://image.tmdb.org/t/p/w300/bBh4WMjARLNPkKV0FuC3a5LsShK.jpg" group-title="Séries | Star+",Mayans M.C. S02 E02
+http://lxls.mx:80/series/994153115/101217/207710.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S02 E03" tvg-logo="http://lxls.mx:8080/images/993e6e59ba2485331917ac78353e32f7.jpg" group-title="Séries | Star+",Mayans M.C. S02 E03
+http://lxls.mx:80/series/994153115/101217/207709.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S02 E04" tvg-logo="http://lxls.mx:8080/images/993e6e59ba2485331917ac78353e32f7.jpg" group-title="Séries | Star+",Mayans M.C. S02 E04
+http://lxls.mx:80/series/994153115/101217/207708.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S02 E05" tvg-logo="https://image.tmdb.org/t/p/w300/yQlKmaeOFilzUGNkwiTHeGKCiAa.jpg" group-title="Séries | Star+",Mayans M.C. S02 E05
+http://lxls.mx:80/series/994153115/101217/207707.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S02 E06" tvg-logo="https://image.tmdb.org/t/p/w300/Z6cSAFuZWRwoi6062fdX5PsTdO.jpg" group-title="Séries | Star+",Mayans M.C. S02 E06
+http://lxls.mx:80/series/994153115/101217/207706.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S02 E07" tvg-logo="https://image.tmdb.org/t/p/w300/oQjqxNQQxiEAslCRRqnb8qoep4D.jpg" group-title="Séries | Star+",Mayans M.C. S02 E07
+http://lxls.mx:80/series/994153115/101217/207705.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S02 E08" tvg-logo="https://image.tmdb.org/t/p/w300/bSzqlW8DeFga70D3d5lOoJIZs30.jpg" group-title="Séries | Star+",Mayans M.C. S02 E08
+http://lxls.mx:80/series/994153115/101217/207188.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S02 E09" tvg-logo="https://image.tmdb.org/t/p/w300/9oiwLYPVjsgP0vgAMWqguliycqP.jpg" group-title="Séries | Star+",Mayans M.C. S02 E09
+http://lxls.mx:80/series/994153115/101217/207187.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S02 E10" tvg-logo="https://image.tmdb.org/t/p/w300/pvsBG4kRJkVAJaCQwVCKJvnHB3H.jpg" group-title="Séries | Star+",Mayans M.C. S02 E10
+http://lxls.mx:80/series/994153115/101217/207186.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S03 E01" tvg-logo="https://image.tmdb.org/t/p/w300/iczlwPwp34zzZjVqusi0IlqAYRI.jpg" group-title="Séries | Star+",Mayans M.C. S03 E01
+http://lxls.mx:80/series/994153115/101217/165591.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S03 E02" tvg-logo="https://image.tmdb.org/t/p/w300/wSXvg4KXjyIYiCF2bJoW4TWqFnJ.jpg" group-title="Séries | Star+",Mayans M.C. S03 E02
+http://lxls.mx:80/series/994153115/101217/165590.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S03 E03" tvg-logo="https://image.tmdb.org/t/p/w300/7MTdB2681alx8bZwo3Arp7T3DSc.jpg" group-title="Séries | Star+",Mayans M.C. S03 E03
+http://lxls.mx:80/series/994153115/101217/165589.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S03 E04" tvg-logo="https://image.tmdb.org/t/p/w300/lQhFReNootgDKw7FAY4tPyQsrnL.jpg" group-title="Séries | Star+",Mayans M.C. S03 E04
+http://lxls.mx:80/series/994153115/101217/165588.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S03 E05" tvg-logo="https://image.tmdb.org/t/p/w300/8WsCVKeTsvk1ff2Lg3Alqd6sRXt.jpg" group-title="Séries | Star+",Mayans M.C. S03 E05
+http://lxls.mx:80/series/994153115/101217/165587.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S03 E06" tvg-logo="https://image.tmdb.org/t/p/w300/pUVApSAvN3Jhm3mAh23hganjHCb.jpg" group-title="Séries | Star+",Mayans M.C. S03 E06
+http://lxls.mx:80/series/994153115/101217/165586.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S03 E07" tvg-logo="https://image.tmdb.org/t/p/w300/8utKxH8mzydFQRbO92sOBJkktiz.jpg" group-title="Séries | Star+",Mayans M.C. S03 E07
+http://lxls.mx:80/series/994153115/101217/165585.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S03 E08" tvg-logo="http://lxls.mx:8080/images/993e6e59ba2485331917ac78353e32f7.jpg" group-title="Séries | Star+",Mayans M.C. S03 E08
+http://lxls.mx:80/series/994153115/101217/165584.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S03 E09" tvg-logo="http://lxls.mx:8080/images/993e6e59ba2485331917ac78353e32f7.jpg" group-title="Séries | Star+",Mayans M.C. S03 E09
+http://lxls.mx:80/series/994153115/101217/165583.mp4
+#EXTINF:-1 tvg-id="" tvg-name="Mayans M.C. S03 E10" tvg-logo="https://image.tmdb.org/t/p/w300/sTakCldL1Vc6TSaXdiq7zIjhwGf.jpg" group-title="Séries | Star+",Mayans M.C. S03 E10
+http://lxls.mx:80/series/994153115/101217/165582.mp4
