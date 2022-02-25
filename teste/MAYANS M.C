3e1e90dd3656ba13b58f05c732f420006d9c70ff
gloriosofg <99756087+gloriosofg@users.noreply.github.com>
2022-02-25 11:34:21 -0300

    Add files via upload

---
+++ teste/MAYANS M.C
@@ -0,0 +1,31 @@
+
+#EXTM3U
+
+#EXTINF:-1 tvg-id="" tvg-name="MAYANS M.C. S01 E01" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/y8PYGJ7TGjtMxwPA5Mhwa9svTRJ.jpg" group-title="SERIE- MAYANS M.C",MAYANS M.C. S01 E01
+http://cdnrede.com:80/series/rosanapin/rosana@123/168842.mp4
+#EXTINF:-1 tvg-id="" tvg-name="MAYANS M.C. S01 E02" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/y8PYGJ7TGjtMxwPA5Mhwa9svTRJ.jpg" group-title="SERIE- MAYANS M.C",MAYANS M.C. S01 E02
+http://cdnrede.com:80/series/rosanapin/rosana@123/168843.mp4
+#EXTINF:-1 tvg-id="" tvg-name="MAYANS M.C. S01 E03" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/y8PYGJ7TGjtMxwPA5Mhwa9svTRJ.jpg" group-title="SERIE- MAYANS M.C",MAYANS M.C. S01 E03
+http://cdnrede.com:80/series/rosanapin/rosana@123/168844.mp4
+#EXTINF:-1 tvg-id="" tvg-name="MAYANS M.C. S01 E04" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/y8PYGJ7TGjtMxwPA5Mhwa9svTRJ.jpg" group-title="SERIE- MAYANS M.C",MAYANS M.C. S01 E04
+http://cdnrede.com:80/series/rosanapin/rosana@123/168845.mp4
+#EXTINF:-1 tvg-id="" tvg-name="MAYANS M.C. S01 E05" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/y8PYGJ7TGjtMxwPA5Mhwa9svTRJ.jpg" group-title="SERIE- MAYANS M.C",MAYANS M.C. S01 E05
+http://cdnrede.com:80/series/rosanapin/rosana@123/168846.mp4
+#EXTINF:-1 tvg-id="" tvg-name="MAYANS M.C. S01 E06" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/y8PYGJ7TGjtMxwPA5Mhwa9svTRJ.jpg" group-title="SERIE- MAYANS M.C",MAYANS M.C. S01 E06
+http://cdnrede.com:80/series/rosanapin/rosana@123/168847.mp4
+#EXTINF:-1 tvg-id="" tvg-name="MAYANS M.C. S01 E07" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/y8PYGJ7TGjtMxwPA5Mhwa9svTRJ.jpg" group-title="SERIE- MAYANS M.C",MAYANS M.C. S01 E07
+http://cdnrede.com:80/series/rosanapin/rosana@123/168848.mp4
+#EXTINF:-1 tvg-id="" tvg-name="MAYANS M.C. S01 E08" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/y8PYGJ7TGjtMxwPA5Mhwa9svTRJ.jpg" group-title="SERIE- MAYANS M.C",MAYANS M.C. S01 E08
+http://cdnrede.com:80/series/rosanapin/rosana@123/168849.mp4
+#EXTINF:-1 tvg-id="" tvg-name="MAYANS M.C. S01 E09" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/y8PYGJ7TGjtMxwPA5Mhwa9svTRJ.jpg" group-title="SERIE- MAYANS M.C",MAYANS M.C. S01 E09
+http://cdnrede.com:80/series/rosanapin/rosana@123/168850.mp4
+#EXTINF:-1 tvg-id="" tvg-name="MAYANS M.C. S01 E10" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/y8PYGJ7TGjtMxwPA5Mhwa9svTRJ.jpg" group-title="SERIE- MAYANS M.C",MAYANS M.C. S01 E10
+http://cdnrede.com:80/series/rosanapin/rosana@123/168851.mp4
+#EXTINF:-1 tvg-id="" tvg-name="MAYANS M.C. S02 E01" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/y8PYGJ7TGjtMxwPA5Mhwa9svTRJ.jpg" group-title="SERIE- MAYANS M.C",MAYANS M.C. S02 E01
+http://cdnrede.com:80/series/rosanapin/rosana@123/168852.mp4
+#EXTINF:-1 tvg-id="" tvg-name="MAYANS M.C. S02 E02" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/y8PYGJ7TGjtMxwPA5Mhwa9svTRJ.jpg" group-title="SERIE- MAYANS M.C",MAYANS M.C. S02 E02
+http://cdnrede.com:80/series/rosanapin/rosana@123/168853.mp4
+#EXTINF:-1 tvg-id="" tvg-name="MAYANS M.C. S02 E03" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/y8PYGJ7TGjtMxwPA5Mhwa9svTRJ.jpg" group-title="SERIE- MAYANS M.C",MAYANS M.C. S02 E03
+http://cdnrede.com:80/series/rosanapin/rosana@123/168854.mp4
+#EXTINF:-1 tvg-id="" tvg-name="MAYANS M.C. S02 E04" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/y8PYGJ7TGjtMxwPA5Mhwa9svTRJ.jpg" group-title="SERIE- MAYANS M.C",MAYANS M.C. S02 E04
+http://cdnrede.com:80/series/rosanapin/rosana@123/168855.mp4
